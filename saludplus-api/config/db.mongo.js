44e128dbaf12c555a218e049043513fa1004c2ed
s4turn <davidbb0003@gmail.com>
2026-03-01 22:18:20 -0500

    Fix: remove deprecated mongoose options to support v9 and added start/dev scripts

--- saludplus-api/config/db.mongo.js
+++ saludplus-api/config/db.mongo.js
@@ -6,10 +6,7 @@ const connectMongo = async () => {
         // AQUI VA LA NUEVA CADENA DE CONEXION MONGO SI CAMBIAS DE BASE DE DATOS
         // EJEMPLO: await mongoose.connect('mongodb://localhost:27017/mi_nueva_bd');
         const connectionString = process.env.MONGO_URI || 'mongodb://localhost:27017/saludplus';
-        await mongoose.connect(connectionString, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        await mongoose.connect(connectionString);
         console.log('MongoDB Connected to:', connectionString);
     } catch (error) {
         console.error('MongoDB Connection Error:', error);
